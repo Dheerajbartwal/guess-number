1bcd83f9faba0b949826fb1f4f54377131584d19
Dheeraj singh <111330690+Dheerajbartwal@users.noreply.github.com>
2022-08-16 15:31:25 +0530

    Add files via upload

---
+++ guess_number project.py
@@ -0,0 +1,14 @@
+# create a python program to guess a Number between 1 to 9.
+
+
+
+import random
+target_num, guess_num = random.randint(1,10),0
+while target_num != guess_num:
+    guess_num = int(input("Guess a number between 1 and 10 until you get it right : "))
+print("Well guesssed")
+
+word = input("enter your word : ")
+print("your word is ", word)
+reversed.word()
+print("your reversed word is ", reversed.word)
